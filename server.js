25fbfc814c8400de467d51934f387b3d18a2ca3b
nitish21 <nitish21390@gmail.com>
2016-03-31 21:53:29 -0400

    Assignment 4 ..

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
 
-var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/form-maker');
-
 var bodyParser    = require('body-parser');
 var multer        = require('multer');
+
+var mongoose = require("mongoose");
+
+var connectionString = 'mongodb://localhost/form-maker';
+
+
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
